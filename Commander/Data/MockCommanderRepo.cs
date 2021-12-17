5b3a8b741f2213f999bbcd99b83f98cd091f528f
Krzysztof <krzysztof.hermaszewski.bee@outlook.com>
2021-12-17 23:23:10 +0100

    Interface

---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,25 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppsCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" },
+                new Command { Id = 1, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" },
+                new Command { Id = 2, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" }
+            };
+        }
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" };
+        }
+    }
+}
