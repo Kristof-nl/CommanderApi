e990d0a00c2043a1681dbab6a9f3409163339530
Krzysztof <krzysztof.hermaszewski.bee@outlook.com>
2021-12-18 16:01:12 +0100

    Add first controllers

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,33 @@
+﻿using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppsCommands();
+
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]   
+        public ActionResult<Command> GetCommandByID(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            return Ok(commandItem);
+        }
+    }
+}
